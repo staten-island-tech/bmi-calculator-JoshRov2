a8ba34cdda6043f5a51c4a43ead346f51d18df92
JoshRov2 <joshr16@nycstudents.net>
2023-10-04 09:24:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-function test(connected) {
-    console.log("connected");
+function getBMI(){
+    const weight = prompt('Enter your weight, in kilograms.');
+    const height = prompt('Enter the square of your height, in centimeters.');
+    calcBMI(weight,height);
+    console.log(calcBMI(weight,height));
 }
-test("connected")
+getBMI();
+
